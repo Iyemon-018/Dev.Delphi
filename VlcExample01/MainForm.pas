97bf3e1ec9dfacdfec2e225a1a2c1b65e438580b
iyemon018@gmail.com <iyemon018>
2019-02-17 15:10:17 +0900

    コードフォーマット

--- VlcExample01/MainForm.pas
+++ VlcExample01/MainForm.pas
@@ -3,8 +3,8 @@ unit MainForm;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
 
 type
   TForm1 = class(TForm)
@@ -22,3 +22,4 @@ implementation
 {$R *.dfm}
 
 end.
+
